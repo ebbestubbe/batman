5dc659c22f0a480869621ed5c9c909f728ef59a9
EbbeS <stubbeebbe@gmail.com>
2017-04-25 12:35:43 +0200

    first draft

--- cg_batman.py
+++ cg_batman.py
@@ -10,13 +10,88 @@ def main():
     n = int(input())  # maximum number of turns before game over.
     x0, y0 = [int(i) for i in input().split()]
     
-   
+    xmin = 0
+    xmax = w-1
+    testingx = True
+    turn = 0
+    x = x0
+    
     while True:
-       
-        bomb_dir = input()  # Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
-        turn+=1
-            
         
+        bomb_dir = input()  # Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
+        eprint("current x: " + str(x))
+        turn+=1
+        if(turn==1):
+            m = (xmin+xmax-1)/2
+            xold = x
+            x = x + 2*(m-x)
+            eprint("initially jumpint to: " + str(x))
+            print(int(x),int(y0))
+
+        
+        if(testingx == True and turn>1):
+            m = (x + xold)/2 #The actual divider line
+            
+            if(bomb_dir == "SAME"):
+                
+                xcorrect = m
+                eprint("correct x at: " + str(x))
+                testingx = False
+            if(bomb_dir == "COLDER"):
+                if(x < xold): xmin = int(m)
+                if(x > xold): xmax = int(m)
+            if(bomb_dir == "WARMER"):
+                if(x < xold): xmax = int(m)
+                if(x > xold): xmin = int(m)
+            eprint("new limits: " + str(xmin) +  " to " + str(xmax))
+            m = (xmin + xmax)/2 #Tentative m for the best cut
+            eprint("best cut at " + str(m))
+            xold = x
+            x = x + 2*(m - x)#Tentative x
+            eprint("best x: " + str(x))
+            if(x < 0): x = 0
+            if(x > w - 1): x = w-1 
+            eprint("corrected x: " + str(x))
+            print(int(x),int(y0))
+    '''
+    while True:
+        
+        bomb_dir = input()  # Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
+        eprint("current x: " + str(x))
+        turn+=1
+        if(turn==1):
+            m = (xmin+xmax-1)/2
+            xold = x
+            x = x + 2*(m-x)
+            eprint("initially jumpint to: " + str(x))
+            print(int(x),int(y0))
+
+        
+        if(testingx == True and turn>1):
+            m = (x + xold)/2 #The actual divider line
+            
+            if(bomb_dir == "SAME"):
+                
+                xcorrect = m
+                eprint("correct x at: " + str(x))
+                testingx = False
+            if(bomb_dir == "COLDER"):
+                if(x < xold): xmin = int(m)
+                if(x > xold): xmax = int(m)
+            if(bomb_dir == "WARMER"):
+                if(x < xold): xmax = int(m)
+                if(x > xold): xmin = int(m)
+            eprint("new limits: " + str(xmin) +  " to " + str(xmax))
+            m = (xmin + xmax)/2 #Tentative m for the best cut
+            eprint("best cut at " + str(m))
+            xold = x
+            x = x + 2*(m - x)#Tentative x
+            eprint("best x: " + str(x))
+            if(x < 0): x = 0
+            if(x > w - 1): x = w-1 
+            eprint("corrected x: " + str(x))
+            print(int(x),int(y0))        
+    '''  
 def eprint(*args,**kwargs):
     print(*args,file=sys.stderr,**kwargs)
 
